ee8f3e062d5540324801ff83cd696822507845f4
Phyo Pyae Sone <56728845+phyo17@users.noreply.github.com>
2022-01-31 21:36:37 +0630

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -1,5 +1,4 @@
-void main () {
-  var var1 = 'hello';
+var var1 = 'hello';
   bool TF = true;
   dynamic var2 = 2.5;
 
@@ -21,4 +20,13 @@ void main () {
   var list1 = [var1, TF, var2];
   for (final i in list1)
     print(i);
-}
+
+  String str = 'this is dart tour examples';
+  print(str);
+
+  int val = 10;
+  double val1 = 1.0;
+  var2 = null;
+
+  print('integer val is $val and float num is $val1.');
+  print('dynamic variable var2 is $var2');
